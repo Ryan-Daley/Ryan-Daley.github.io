13a879a49ec6020ae9f0334d776418145203f2ab
RD <113039811+Ryan-Daley@users.noreply.github.com>
2023-10-09 20:57:19 +0100

    chore - updating config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  website: "https://ryan-daley.github.io/Website/",
+  website: "https://ryan-daley.github.io/",
   title: "Ryan Daley",
   desc: "Ryan's Porfolio.",
   author: "Ryan Daley"
@@ -8,14 +8,14 @@ export const SITE = {
 export const THEME = {
   editLink: {
     pattern:
-      "https://github.com/Renovamen/renovamen.github.io/edit/main/src/content/blog:path",
+      "https://github.com/Ryan-Daley/Ryan-Daley.github.io/edit/main/src/content/blog:path",
     text: "Edit this page on GitHub"
   },
   lastUpdatedText: "Last updated:"
 };
 
 export const GISCUS = {
-  repo: "This-is-an-Apple/giscus-zxh.io",
+  repo: "This-is-an-Apple/giscus-ryandaleyanalytics.com",
   repoId: "R_kgDOGl2SjQ",
   category: "Announcements",
   categoryId: "DIC_kwDOGl2Sjc4CAcxK",
